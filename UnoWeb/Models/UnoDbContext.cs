bf49106b349aac8e169f3e3750f47e20f35ba33b
Matthew Kirkendall <kirk.viola@gmail.com>
2021-11-01 12:04:16 -0400

    Add project files.

---
+++ UnoWeb/Models/UnoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnoWeb.Models
+{
+    public class UnoDbContext : DbContext
+    {
+        // Tables go here
+
+        public UnoDbContext(DbContextOptions<UnoDbContext> options)
+            : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // build tables here if fluent api used
+        }
+    }
+}

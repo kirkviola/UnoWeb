72072b4b0f8c7e70fd4ccfebb669e66f9495a321
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-11-01 12:22:33 -0400

    game init commit

---
+++ UnoWeb/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Game
+{
+    public int id { get; set; }
+    public string GameRoom { get; set; }
+    public Queue<Card> DealingDeck { get; set; }
+    public Queue<Card> DiscardDeck { get; set; }
+    public Queue<Player> Players { get; set; }
+    public Player active { get; set; }
+
+    public Game()
+	{
+	}
+}

a109c3ac7289543ede3b1a793e25d52c98637132
Matthew Kirkendall <kirk.viola@gmail.com>
2021-11-02 10:32:42 -0400

    fixed java dog's lowercase Id

--- UnoWeb/Models/Game.cs
+++ UnoWeb/Models/Game.cs
@@ -5,13 +5,12 @@ using UnoWeb.Models;
 
 public class Game
 {
-    
     public int Id { get; set; }
     public string GameRoom { get; set; }
-    public int PlayerCap { get; set; }
-    
-    public virtual Player Active { get; set; }    
-    public virtual IEnumerable<GameCard> GameCards { get; set; }
+    public Queue<Card> DealingDeck { get; set; }
+    public Queue<Card> DiscardDeck { get; set; }
+    public Queue<Player> Players { get; set; }
+    public Player active { get; set; }
 
     public Game()
 	{
